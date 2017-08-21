c938bd314a94e04600c58db2fa7821f6827e65a0
Adam Buckley <adam@2pisoftware.com>
2017-08-21 11:57:45 +1000

    Call setChecked in setValue

--- system/classes/html/form/inputfield/Checkbox.php
+++ system/classes/html/form/inputfield/Checkbox.php
@@ -9,4 +9,17 @@ class Checkbox extends \Html\Form\InputField {
 	
 	public $type = "checkbox";
 	
+	/**
+	 * setValue override to instead call setChecked
+	 * 
+	 * @param Mixed $value checked value, set to a truthy value to be checked (1, true, 'yes', etc.)
+	 */
+	public function setValue($value) {
+		if (!!$value) {
+			$this->setChecked('checked');
+		}
+
+		return $this;
+	}
+
 }
